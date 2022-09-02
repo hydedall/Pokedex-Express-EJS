94ea6c89c68aeea9ab331faa531ddbe88aaea8f7
hydedall <hyde.dallas1@gmail.com>
2022-09-02 00:04:21 -0400

    server change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 //SETTING DEPENDENCIES
 ///////////////
 const express = require('express');
-const fruits = require("./models/pokemon.js")
+const pokemons = require("./models/pokemon.js")
 const methodOverride = require("method-override")
 
 
